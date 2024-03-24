44e712650f75c2b7848ab1b8f0123add70e2ee57
KostasBzn <kostasbouzianis@gmail.com>
2024-03-24 16:44:48 +0100

    backend test

--- server/verification/galaxyEmails.js
+++ server/verification/galaxyEmails.js
@@ -1,12 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_SERVER,
-  port: process.env.SMTP_PORT,
+  host: "smtp.elasticemail.com",
+  port: "2525",
   secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: "kosb999663@gmail.com",
+    pass: "9CACA1A90386FE08AC394893F7684B0CABFF",
   },
 });
 
